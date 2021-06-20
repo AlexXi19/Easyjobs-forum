a5712f42ea35dbc8285d0962bc37f6309e25c5b1
AlexXi19 <68758451+AlexXi19@users.noreply.github.com>
2021-06-19 22:13:24 -0700

    moved a few comment functions

--- nodeJS/controllers/PostController.js
+++ nodeJS/controllers/PostController.js
@@ -100,48 +100,6 @@ function addPost(req, res) {
   });
 }
 
-// addComment
-function addComment(req, res) {
-  console.log("addComment function called");
-  // Adds comment to comment collection
-  User.find({ email: ssn.email }, function (err, docs) {
-    // Obtaining values from front end
-    var currentUserID = docs[0]._id;
-    var currentContent = req.body.commentContent;
-    var currentReplyToID = req.body.replyToID
-      ? new mongo.ObjectID(req.body.replyToID)
-      : null;
-    var currentPostID = new mongo.ObjectID(req.body.replyPostID);
-
-    // Creating comment object
-    const newComment = new Comment({
-      content: currentContent,
-      userID: currentUserID,
-      postID: currentPostID,
-      replyToID: currentReplyToID,
-    });
-
-    // Save Comment into comments collection
-    newComment.save();
-    console.log("Comment saved");
-
-    // Insert into Post collection
-    Post.find({ _id: currentPostID }, function (err, docs1) {
-      // Adding comment object to post object
-      docs1[0].comments.push(newComment);
-      docs1[0].save();
-      // Incrementing comment number by 1
-      Post.updateOne(
-        { _id: currentPostID },
-        { $inc: { commentsNum: 1 } },
-        function (err, result) {
-          if (err) throw err;
-          console.log("Comment added successfully");
-        }
-      );
-    });
-  });
-}
 
 // getAllPosts
 function getAllPosts(req, res) {
@@ -285,38 +243,14 @@ function updatePost(req, res) {
 // );
 // }
 
-// deleteComment
-function deleteComment(req, res) {
-   console.log("deleteComment function called");
-   var commentID = new mongo.ObjectID(req.body.commentID);
-   var postID = new mongo.ObjectID(req.body.postID);
-   var userID = new mongo.ObjectID(req.body.userID);
-   console.log("Deleting Comment...");
-   console.log(commentID);
-   // Deleting entry in Post collection
-   Post.updateMany(
-     { _id: postID },
-     { $pull: { comments: { _id: commentID } } },
-     function (err, result) {
-       if (err) throw err;
-       // Deleting entry in Comment collection
-       console.log("Comment in Post collection deleted");
-       Comment.deleteOne({ _id: commentID }, function (err, result) {
-         if (err) throw err;
-         console.log("Comment in Comment collection deleted");
-         });
-       });
-     }
 
 // getPostLikeNumber
 
-// getCommentNumber
 
 module.exports = {
   // all functions
   addPost,
   deletePost,
-  addComment,
   getAllPosts,
   getPostByID,
   getUserByPost,
